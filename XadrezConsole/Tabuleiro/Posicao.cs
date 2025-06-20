70cd9660f1fd917be3d8ac946f1d9624da8da7c2
Igor Bonfim <igorbonfim88@gmail.com>
2025-06-19 21:21:09 -0300

    Testes inicias com movimentos possíveis das peças

--- XadrezConsole/Tabuleiro/Posicao.cs
+++ XadrezConsole/Tabuleiro/Posicao.cs
@@ -10,6 +10,12 @@ public class Posicao
         this.coluna = coluna;
     }
 
+    public void definirValores(int linha, int coluna)
+    {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
     public override string ToString()
     {
         return linha +
