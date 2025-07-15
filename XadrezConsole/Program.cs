5d7c2678e0cb55220573883c60533335bbea0b7f
Igor Bonfim <igorbonfim88@gmail.com>
2025-07-14 23:52:12 -0300

    Implementando a lógica de xequemate

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -34,13 +34,16 @@ namespace XadrezConsole
                         partida.validarPosicaoDestino(origem, destino);
 
                         partida.realizaJogada(origem, destino);
+                        
                     }
                     catch (TabuleiroException e)
                     {
                         Console.WriteLine(e.Message);
                         Console.ReadLine();
                     }
-                }            
+                }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
             catch (TabuleiroException e)
             {
--- XadrezConsole/xadrez/PartidaDeXadrez.cs
+++ XadrezConsole/xadrez/PartidaDeXadrez.cs
@@ -70,6 +70,11 @@ class PartidaDeXadrez
             xeque = false;
         }
 
+        if (testeXequeMate(adversaria(jogadorAtual)))
+        {
+            terminada = true;
+        }
+
         turno++;
         mudaJogador();
     }
@@ -182,6 +187,39 @@ class PartidaDeXadrez
             }
         }
         return false;
+    }
+
+    public bool testeXequeMate(Cor cor)
+    {
+        if (!estaEmXeque(cor))
+        {
+            return false;
+        }
+
+        foreach (Peca x in pecasEmJogo(cor))
+        {
+            bool[,] mat = x.movimentosPossiveis();
+            for (int i = 0; i<tab.linhas; i++)
+            {
+                for (int j = 0; j<tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        Posicao origem = x.posicao;
+                        Posicao destino = new Posicao(i, j);
+                        Peca pecaCapturada = executaMovimento(origem, destino);
+                        bool testeXeque = estaEmXeque(cor);
+                        desfazMovimento(origem, destino, pecaCapturada);
+
+                        if (!testeXeque)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     public void colocarNovaPeca(char coluna, int linha, Peca peca)
